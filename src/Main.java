bf8c0219fcd990b6d3eb33d446f2424ac653e5ca
Roshan Raj <roshan.raj@frogforce503.org>
2018-10-07 21:04:54 -0400

    Optimized for jar extraction

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-	static Config config = new Config(0.050000000000000, 50, 10);
-	static final double wheelbase_width = 24.0; // 24 inches apart
-
-	public static void main(String[] args) {
-		WheelTrajectory traj = StraightProfileGenerator.generateTrajectory(config, wheelbase_width, -90);
-	}
-}
--- src/WheelTrajectory.java
+++ src/WheelTrajectory.java
@@ -40,32 +40,4 @@ public class WheelTrajectory {
 	public Trajectory getRightTrajectory() {
 		return trajectories[1];
 	}
-
-	public void graphForwardvsHorizontal() {
-		double[] forward = new double[getLeftTrajectory().length()];
-		double[] horizontal = new double[getLeftTrajectory().length()];
-		double[] forward2 = new double[getRightTrajectory().length()];
-		double[] horizontal2 = new double[getRightTrajectory().length()];
-		for (int i = 0; i < forward.length; i++) {
-			forward[i] = getLeftTrajectory().get(i).getForward();
-			horizontal[i] = getLeftTrajectory().get(i).getHorizontal();
-			forward2[i] = getRightTrajectory().get(i).getForward();
-			horizontal2[i] = getRightTrajectory().get(i).getHorizontal();
-		}
-
-		new Graph("Horizontal", "Forward", horizontal, forward, horizontal2, forward2).run();
-	}
-
-	public void graphVelocities() {
-		double[] time = new double[getLeftTrajectory().length()];
-		double[] velLeft = new double[getLeftTrajectory().length()];
-		double[] velRight = new double[getRightTrajectory().length()];
-		for (int i = 0; i < time.length; i++) {
-			time[i] = getLeftTrajectory().get(i).getTime();
-			velLeft[i] = getLeftTrajectory().get(i).getVelocity();
-			velRight[i] = getRightTrajectory().get(i).getVelocity();
-		}
-
-		new Graph("Velocity", "Time", time, velLeft, time, velRight).run();
-	}
 }
